5f7de88bad0a44f6f2223f54d83971286204c70b
aryajason <aryajasonr@gmail.com>
2024-11-14 15:23:14 +0700

    Update mainTest.cpp

--- Number 3/mainTest.cpp
+++ Number 3/mainTest.cpp
@@ -26,13 +26,18 @@ for(int i=0; i<7; i++)
 		}
 	}
 
-Student Check[7] = {{9950310962, "Ronaldo Valentino Uneputty", 80},
-						{9952382180, "Ari Lutfianto", 65},
-						{9960312699, "Handi Ramadhan", 90},
-						{9963959682, "Rio Alfandra", 55},
-						{9965653989, "Joko", 60},
-						{9970272750, "Joko", 60},
-						{9970293945, "Alivia Rahma Pramesti", 70}};
+Student Check[7] = {                           
+
+	{9960312699, "Handi Ramadhan", 90},
+        {9963959682, "Rio Alfandra", 55},
+        {9950310962, "Ronaldo Valentino Uneputty", 80},
+        {9970272750, "Joko", 60},
+        {9970293945, "Alivia Rahma Pramesti", 70},
+        {9952382180, "Ari Lutfianto", 65},
+        {9965653989, "Joko", 60},
+
+	
+};
 
 bool flag=0;
   
